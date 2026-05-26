b86a97d96d81adf5717ccb74524088f2f1064fb2
Developer <2714824419@qq.com>
2026-05-27 01:22:04 +0800

    fix: correct CSS comment syntax in App.vue style block
    Was using /* without proper comment syntax which could cause parsing issues.

--- App.vue
+++ App.vue
@@ -7,6 +7,9 @@
 			console.log('页面数量:', getCurrentPages().length)
 			// 初始化默认数据
 			initAllData()
+			// 验证 CSS 变量 - 使用 dom 获取 computed style
+			const bgColor = '#FDF4E9'
+			console.log('[App] 背景色变量验证:', bgColor)
 		},
 		onShow: function() {
 			console.log('App Show')
@@ -18,6 +21,6 @@
 </script>
 
 <style>
-/每个页面公共css */
+/* 每个页面公共css */
 @import "./common/style.scss";
 </style>
